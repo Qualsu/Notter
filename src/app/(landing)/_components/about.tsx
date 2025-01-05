3f77163b61acd514e1e0aaa3535fb5f5ca26f76c
ItzKeeni <maratkanovartem89@mail.ru>
2025-01-06 00:20:30 +0300

    add dark icon

--- src/app/(landing)/_components/about.tsx
+++ src/app/(landing)/_components/about.tsx
@@ -2,6 +2,10 @@ import lamp from "../../../../public/icon/lamp.svg"
 import people from "../../../../public/icon/people.svg"
 import options from "../../../../public/icon/options.svg"
 import sync from "../../../../public/icon/ecosystem.svg"
+import lampDark from "../../../../public/icon/lampDark.svg"
+import peopleDark from "../../../../public/icon/peopleDark.svg"
+import optionsDark from "../../../../public/icon/optionsDark.svg"
+import syncDark from "../../../../public/icon/ecosystemDark.svg"
 import { Card } from "./card"
 
 export function About(){
@@ -11,21 +15,25 @@ export function About(){
                 name="Просто и удобно"
                 description="Мы предоставляем простое, удобное, а главное бесплатное приложения"
                 img={lamp}
+                imgDark={lampDark}
             />
             <Card
                 name="Функциональность"
                 description="Большой функционал которого должно хватить для простых и средних задач"
                 img={options}
+                imgDark={optionsDark}
             />
             <Card
                 name="Работа в команде"
                 description="В Notter присутствует поддержка организаций из Qual ID для работы с командой"
                 img={people}
+                imgDark={peopleDark}
             />
             <Card
                 name="Синхронизация"
                 description="Синхронизируя данные, вы можете работать с любого устройства"
                 img={sync}
+                imgDark={syncDark}
             />
         </>
     )
--- src/app/(landing)/_components/card.tsx
+++ src/app/(landing)/_components/card.tsx
@@ -4,9 +4,10 @@ interface CardProps {
     name: string
     description: string
     img: any
+    imgDark: any
 }
 
-export function Card({ name, description, img }: CardProps){    
+export function Card({ name, description, img, imgDark }: CardProps){    
     return (
         <div className="flex justify-center items-center bg-zinc-100/60 dark:bg-zinc-900/25 rounded-xl max-w-[1000px] w-full mx-2 py-1 z-[-100]">
             <section className="flex justify-center md:flex-row flex-col items-center my-8">
@@ -14,7 +15,8 @@ export function Card({ name, description, img }: CardProps){
                     <h1 className="text-4xl my-5 md:my-2">{name}</h1>
                     <h3 className="text-xl break-word max-w-[500px] px-4 md:px-0 w-96">{description}</h3>
                 </div>
-                <Image src={img} width="200" height="200" alt="" className="m-2 md:mt-0 mt-5"/>
+                <Image src={img} width="200" height="200" alt="" className="hidden dark:block m-2 md:mt-0 mt-5"/>
+                <Image src={imgDark} width="200" height="200" alt="" className="block dark:hidden m-2 md:mt-0 mt-5 opacity-20"/>
             </section>
         </div>
     )
