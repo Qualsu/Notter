b1b185906b575847a7be775fbafe2bb000053047
ItzKeeni <maratkanovartem89@mail.ru>
2025-01-05 23:47:41 +0300

    fix landing

--- src/app/(landing)/_components/card.tsx
+++ src/app/(landing)/_components/card.tsx
@@ -8,11 +8,11 @@ interface CardProps {
 
 export function Card({ name, description, img }: CardProps){    
     return (
-        <div className="flex justify-center items-center bg-zinc-900/25 rounded-xl max-w-[1000px] w-full mx-2 py-1 z-[-100]">
+        <div className="flex justify-center items-center bg-zinc-100/60 dark:bg-zinc-900/25 rounded-xl max-w-[1000px] w-full mx-2 py-1 z-[-100]">
             <section className="flex justify-center md:flex-row flex-col items-center my-8">
                 <div className="text-center md:text-left">
-                    <h1 className="text-4xl text-white my-5 md:my-2">{name}</h1>
-                    <h3 className="text-xl break-word max-w-[500px] text-gray-300 px-4 md:px-0 w-96">{description}</h3>
+                    <h1 className="text-4xl my-5 md:my-2">{name}</h1>
+                    <h3 className="text-xl break-word max-w-[500px] px-4 md:px-0 w-96">{description}</h3>
                 </div>
                 <Image src={img} width="200" height="200" alt="" className="m-2 md:mt-0 mt-5"/>
             </section>
